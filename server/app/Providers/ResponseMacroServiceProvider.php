116a07f22105048d55a8d7ff917856693ad0837c
Sagynbek <kenzhebaevsagynbek@gmail.com>
2020-11-28 13:52:01 +0300

    Macro service added for standartized response

---
+++ server/app/Providers/ResponseMacroServiceProvider.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\Response;
+use Illuminate\Support\ServiceProvider;
+
+class ResponseMacroServiceProvider extends ServiceProvider
+{
+  /**
+   * Register the application's response macros.
+   *
+   * @return void
+   */
+  public function boot()
+  {
+
+    Response::macro('success', function ($data, $message = null, $status = 200) {
+      $result = [
+        'message' => [
+          'status'  =>  true,
+          'message' =>  $message,
+        ],
+        'data'     =>  $data,
+      ];
+      return Response::json($result, $status);
+    });
+
+    Response::macro('error', function ($message, $status = 400) {
+      $result = [
+        'message' => [
+          'status'  =>  false,
+          'message' =>  $message,
+        ],
+        'data'     =>  [],
+      ];
+      return Response::json($result, $status);
+    });
+  }
+}
